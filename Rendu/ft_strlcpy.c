c697d4273debdec2b4f2b6e4493c8e9b4fd149cc
Andyh Fahasoavana Manambina Rabefamantanatsoa <arabefam@c1r6s5.42antananarivo.mg>
2024-02-20 16:11:44 +0300

    Not tested yet

---
+++ Rendu/ft_strlcpy.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arabefam <arabefam@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/20 12:53:44 by arabefam          #+#    #+#             */
+/*   Updated: 2024/02/20 14:10:11 by arabefam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stddef.h>
+// #include <bsd/string.h>
+
+static size_t	st_strlen(const char *str)
+{
+	size_t	count;
+
+	count = -1;
+	while (str[++count] != '\0')
+		;
+	return (count);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	while (src[i] != '\0' && i < size - 1)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (st_strlen(src));
+}
+
+int	main()
+{
+	char	str[] = "Andyh";
+	char	dest[] = "Bonjour";
+	size_t	size = ft_strlcpy(dest, str, 48);
+	printf("%zu\n", size);
+	printf("%s\n", dest);
+}
