abe109e3bfd12b4f54238f31c554c6f5aa8936bb
Andyh Fahasoavana Manambina Rabefamantanatsoa <arabefam@c1r6s5.42antananarivo.mg>
2024-02-20 16:10:09 +0300

    Done for ft_memcpy

---
+++ Rendu/ft_memcpy.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arabefam <arabefam@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/20 07:15:44 by arabefam          #+#    #+#             */
+/*   Updated: 2024/02/20 15:54:21 by arabefam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+void *ft_memcpy(void *dest, const void *src, size_t n)
+{
+	int	i;
+
+	i = -1;
+	while (i++, n-- > 0)
+		((unsigned char *) dest)[i] = ((unsigned char *) src)[i];
+	return (dest);
+}
+
+void print_elements(char *array, int size)
+{
+  int i;
+  printf ("Elements : ");
+  for (i = 0; i < size; i++) {
+    printf ("%c, ", array[i]);
+  }
+  printf ("\n");
+}
+
+int main(void)
+{
+  char c_array[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+  print_elements(c_array, sizeof(c_array));
+  printf("After memcpy()\n");
+  ft_memcpy((void *)&c_array[1], (void *)&c_array[0], 4);
+  print_elements(c_array, sizeof(c_array));
+}
