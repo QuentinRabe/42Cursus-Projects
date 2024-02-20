035253bf07aa17d63416f129b0d7070d7c604f12
Andyh Fahasoavana Manambina Rabefamantanatsoa <arabefam@c1r6s5.42antananarivo.mg>
2024-02-20 16:11:37 +0300

    Not tested yet

---
+++ Rendu/ft_strlcat.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arabefam <arabefam@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/20 14:14:50 by arabefam          #+#    #+#             */
+/*   Updated: 2024/02/20 14:49:59 by arabefam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <bsd/string.h>
+#include <stdio.h>
+#include <stddef.h>
+
+static size_t	st_strlen(const char *str)
+{
+	size_t	count;
+
+	count = -1;
+	while (str[++count] != '\0')
+		;
+	return (count);
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	dst_len;
+	size_t	src_len;
+
+	dst_len = -1;
+	src_len = st_strlen(src);
+	while (dst[++dst_len] != '\0')
+		;
+	i = 0;
+	while (src[i] != '\0' && i < size)
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	dst[dst_len + i] = '\0';
+	printf("dst_len = %zu\n", dst_len);
+	printf("src_len = %zu\n", src_len);
+	if (dst_len >= size)
+		return (size + src_len);
+	else
+		return (dst_len + src_len);
+}
+
+
+int main()
+{
+	char	dest[] = "Hello";
+	char	src[] = " world";
+
+	size_t	size = strlcat(dest, src, 2);
+	printf("%zu\n", size);
+	printf("%s\n", dest);
+}
